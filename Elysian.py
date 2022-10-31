a107e06c54eacfac1df88fb61f8f857a5c7919c3
Caden1234567 <114608129+Caden1234567@users.noreply.github.com>
2022-10-31 09:05:54 -0500

    Create Elysian.py

---
+++ Elysian.py
@@ -0,0 +1,8 @@
+print("Hello You Have Died!")
+print("Welcome To The Afterlife!")
+print("This Is Where You Spend The Rest Of Eternity")
+print(
+  "See All You Humans are The Same Religon and All Of Us Gods Work Together")
+print("So Now That Im Done Giving You An existential Crisis")
+print("")
+input("Press Enter To Continue")
