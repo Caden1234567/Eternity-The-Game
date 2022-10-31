2f59b1e229108f0e4f6c6f83d3643ae1349c6c8d
Caden1234567 <114608129+Caden1234567@users.noreply.github.com>
2022-10-31 09:07:38 -0500

    Create Heaven.py

---
+++ Heaven.py
@@ -0,0 +1,25 @@
+print("Hello You Have Died!")
+print(
+  "See All You Humans are The Same Religon and All Of Us Gods Work Together")
+print("So Now That Im Done Giving You An existential Crisis")
+print("")
+input("Press Enter To Continue")
+print(
+  "You Were Christan one of the most popular religon choices here at life incorperated"
+)
+print(
+  "Welcome To The Eternal Afterlife You Have Been Chosen Very Carefuly And The Godly Counsel Have Desided To Put You In Heaven "
+)
+print("This Is Where You Spend The Rest Of Eternity")
+print("I Will Ask You Some Questions So You Can Have everything You wanted")
+input("Press Enter To Continue")
+House = input("What Is Your Dream House? In Example Say Modern or classic! ")
+print("Ok Here Is The Key To Your " + House + " House")
+Car = input("What Is You Dream Car? Be Extremly Specific ")
+CarColor = input("What Color Do You Want Your " + Car + " To Be? ")
+print("Ok Here Is The Key To Your " + CarColor + " " + Car + " ")
+input("Press Enter To Continue")
+print("I Will Now Teleport You To Your " + House + " House With A " +
+      CarColor + " " + Car + " ")
+print("(You) Wow This Is Exactly How I wanted it ")
+print("")
