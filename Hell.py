fec5df72b176576d9fa3bd198e5cd19234ed6178
Caden1234567 <114608129+Caden1234567@users.noreply.github.com>
2022-10-31 09:08:00 -0500

    Create Hell.py

---
+++ Hell.py
@@ -0,0 +1,15 @@
+print("Hello You Have Died!")
+print("Welcome To The Afterlife!")
+print("This Is Where You Spend The Rest Of Eternity")
+print(
+  "See All You Humans are The Same Religon and All Of Us Gods Work Together")
+print("So Now That Im Done Giving You An existential Crisis")
+print("")
+input("Press Enter To Continue")
+print(
+  "You Were Christan one of the most popular religon choices here at life incorperated"
+)
+print("You Werent that good a person so I am going put you in hell")
+print("")
+print("")
+input("Press Enter To Continue")
